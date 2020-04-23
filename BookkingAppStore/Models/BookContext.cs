1077c6edee00509b6b1b15437fc66b72fb1276f1
Anton Alekseev <antoxaalekseev6@gmail.com>
2020-04-23 23:08:42 +0300

    less4

---
+++ BookkingAppStore/Models/BookContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace BookkingAppStore.Models
+{
+    public class BookContext : DbContext
+    {
+
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book
+            {
+                Name = "Мертвые Души",
+                Author = "Н.Гоголь",
+                Price=220
+            });
+            db.Books.Add(new Book
+            {
+                Name = "Отцы и дети",
+                Author = "И.Тургенев",
+                Price = 180
+            });
+            db.Books.Add(new Book
+            {
+                Name = "Чайка",
+                Author = "А.Чехов",
+                Price = 150
+            });
+            base.Seed(db);
+        }
+    }
+
+}
